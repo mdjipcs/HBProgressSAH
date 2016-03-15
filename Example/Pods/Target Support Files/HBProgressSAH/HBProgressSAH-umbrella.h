69b85f5e33d3e926bf40ff5c9882b1d4246b67bd
Ryu Su <yishanpei@live.cn>
2016-03-16 01:18:12 +0800

    Release 0.1.0-beta

--- Example/Pods/Target Support Files/HBProgressSAH/HBProgressSAH-umbrella.h
+++ Example/Pods/Target Support Files/HBProgressSAH/HBProgressSAH-umbrella.h
@@ -1,7 +1,5 @@
 #import <UIKit/UIKit.h>
 
-#import "MPProgressHUD.h"
-#import "MPProgressView.h"
 
 FOUNDATION_EXPORT double HBProgressSAHVersionNumber;
 FOUNDATION_EXPORT const unsigned char HBProgressSAHVersionString[];
